0642917bf699905bf5924beac5016c05af63ba95
Nishant Tiwari <tnishant710710@gmail.com>
2025-06-26 05:42:53 +0530

    mongodb and express setup in backend

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const connectToMongo = require('./db');
+const express = require('express')
+
+connectToMongo();
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Nishant')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
