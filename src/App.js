a5e4b8ee58316bcec756a918a5a7ed61542ee190
Nishant Tiwari <tnishant710710@gmail.com>
2025-07-03 13:28:36 +0530

    made home, navbar, about components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+import Navbar from './component/Navbar';
+import Home from './component/Home';
+import About from './component/About';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/home' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+      </Routes>
+      </Router>
+    </>
   );
 }
 
